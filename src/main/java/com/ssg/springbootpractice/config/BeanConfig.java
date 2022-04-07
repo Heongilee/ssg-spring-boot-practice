45f93e6935bb08ae5d3d151ec7f2a1aae3227467
Heongilee <gjsrl1@gmail.com>
2022-04-07 15:09:02 +0900

    Demonstrate #4 - bean config by @Component annotation
    2번째 방법

--- src/main/java/com/ssg/springbootpractice/config/BeanConfig.java
+++ src/main/java/com/ssg/springbootpractice/config/BeanConfig.java
@@ -5,7 +5,7 @@ import com.ssg.springbootpractice.service.MyService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration // 이 클래스는 "설정파일"이다. 라고 알려줌
+//@Configuration // 이 클래스는 "설정파일"이다. 라고 알려줌
 public class BeanConfig {
 
     @Bean
